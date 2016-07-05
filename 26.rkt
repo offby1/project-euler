efc2015288146c060e6390cb697812f1291594e7
Eric Hanchrow <eric.hanchrow@gmail.com>
2016-07-04 19:34:55 -0700

    Don't use soegaard/math/math
    It's obsolete and doesn't work

--- 26.rkt
+++ 26.rkt
@@ -1,5 +1,17 @@
 #lang racket
-(require (only-in (planet soegaard/math/math) coprime? order))
+(require (only-in math coprime? with-modulus mod= mod+ mod*))
+
+;; Adapted from
+;; https://planet.racket-lang.org/package-source/soegaard/math.plt/1/5/number-theory.ss
+(define (order g n)
+  (if (not (coprime? g n))
+      (error "In (order g n) the g and n must me coprime")
+      (with-modulus n
+                    (let loop ([k 1]
+                               [a g])
+                      (if (mod= a 1)
+                          k
+                          (loop (mod+ k 1) (mod* a g)))))))
 
 (define (length-of-repeated-fraction denom)
   (if (coprime? 10 denom)
