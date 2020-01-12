46abc5afd392c80ac23ef05c9d92342ce8ff222b
Eric Hanchrow <eric.hanchrow@gmail.com>
2020-01-12 13:50:39 -0800

    Rewrote in Python

---
+++ 05.py
@@ -0,0 +1,13 @@
+# 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+import functools
+import math
+
+def lcm(a, b):
+    return a * b // math.gcd(a, b)
+
+
+print(functools.reduce(lcm, range(1, 11)))
+print(functools.reduce(lcm, range(1, 21)))
--- 05.ss
+++
@@ -1,5 +0,0 @@
-#lang scheme
-
-;; From Eli Barzilay
-
-(apply lcm (build-list 21 add1))
