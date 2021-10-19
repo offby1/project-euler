a6d3e93e68e69a014eb45abe88aa7e9bd6bc0326
Eric Hanchrow <eric.hanchrow@gmail.com>
2021-10-18 17:42:40 -0700

    Not sure what I was doing here

--- 04.py
+++ 04.py
@@ -5,15 +5,16 @@
 # Find the largest palindrome made from the product of two 3-digit numbers.
 
 import itertools
+import operator
 
 three_digit_numbers = range(100, 1000)
-products = (a * b for a, b in itertools.product(three_digit_numbers, three_digit_numbers))
+products = ((a * b, a, b) for a, b in itertools.product(three_digit_numbers, three_digit_numbers))
 
 def is_panindromic(n):
     n = str(n)
     return n == ''.join(reversed(n))
 
-palindromic_products = (p for p in products if is_panindromic(p))
+palindromic_products = (p for p in products if is_panindromic(p[0]))
 
-print(max(palindromic_products))
+print(max(palindromic_products, key=operator.itemgetter(0)))
 # >>> 906609
