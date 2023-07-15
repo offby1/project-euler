da391083c1ee21e1ab0293919d602de76d5d49f5
Eric Hanchrow <eric.hanchrow@gmail.com>
2023-07-15 09:04:34 -0700

    seems like it otta work, and yet ...
    ... it's taking forever

--- 61.py
+++ 61.py
@@ -1,3 +1,5 @@
+import dataclasses
+import functools
 import itertools
 
 
@@ -9,17 +11,27 @@ def P(s: int):
     return formula
 
 
+@functools.cache
 def polygonals_of_size(s: int):
+    rv = []
     for n in itertools.count(1):
-        yield P(s)(n)
+        p = P(s)(n)
+        if p > 9999:
+            return rv
+        rv.append (p)
 
 
+@functools.cache
 def four_digit_polygonals(s: int):
+    rv = []
+
     for t in polygonals_of_size(s):
         if len(str(t)) == 4:
-            yield t
+            rv.append(t)
         elif len(str(t)) > 4:
-            return
+            break
+
+    return rv
 
 
 def overlaps(head: int, tail: int) -> bool:
@@ -27,22 +39,44 @@ def overlaps(head: int, tail: int) -> bool:
     assert 1000 <= tail <= 9999
     if head == tail:
         return False
-    return str(tail).startswith(str(head)[-2])
+    last_two_of_head = str(head)[-2:]
+    first_two_of_tail = str(tail)[0:2]
+    return last_two_of_head == first_two_of_tail
+
+
+@dataclasses.dataclass(frozen=True)
+class Solution:
+    sizes_and_fdns: list[tuple[int, int]]
+
+    # def __post_init__(self):
+    #     assert [3 <= size <= 8 for size, _ in self.sizes_and_fdns]
+    #     assert [1000 <= fdn <= 9999 for _, fdn in self.sizes_and_fdns]
+
+    #     self.sanity_check()
+
+    def union(self, size: int, fdn: int):
+        assert 1000 <= fdn <= 9999
+        assert [size > s for s, _ in self.sizes_and_fdns]
+
+        return Solution(sizes_and_fdns=self.sizes_and_fdns + [(size, fdn)])
+
+    def sanity_check(self):
+        for size, fdn in self.sizes_and_fdns:
+            assert fdn in polygonals_of_size(size)
+            print(f"{self} is legit")
 
 
 def solutions_from(s: int):
     for candidate in four_digit_polygonals(s):
+        # print(f"{'*' * s}: {candidate=}")
         if s == 8:
-            yield {candidate}
+            yield Solution(sizes_and_fdns=[(s, candidate)])
             return
         assert s < 8, f"wtf {s=}"
         for sol in solutions_from(s + 1):
-            for number in sol:
-                if overlaps(candidate, number):
-                    yield sol.union([candidate])
-
-
-s = solutions_from(3)
+            for _, fdn in sol.sizes_and_fdns:
+                if overlaps(candidate, fdn):
+                    yield sol.union(s, candidate)
 
 
 def test_formula():
@@ -57,3 +91,7 @@ def test_overlaps():
     assert overlaps(1234, 3456)
     assert not overlaps(3456, 1234)
     assert not overlaps(1111, 1111)
+
+
+if __name__ == "__main__":
+    print(list(solutions_from(3)))
