7d751ccc896c824cc329a365170db6c01e939add
Eric Hanchrow <eric.hanchrow@gmail.com>
2023-07-15 15:00:35 -0700

    Use new NodeType instead of Any

--- 60/bfs.py
+++ 60/bfs.py
@@ -2,23 +2,24 @@ from typing import Any, Callable, Iterable
 
 from sympy.combinatorics import GrayCode
 
-Neighbors = Iterable[Any]
+NodeType = Any
+Neighbors = Iterable[NodeType]
 
 
 def breadth_first_search(
     *,
-    starting_datum: Any,
-    get_neighbors: Callable[[Any], Neighbors],
-    per_datum_work: Callable[[Any], None],
-    stopping_criterion: Callable[[Any], bool],
-) -> Iterable[Any]:
+    starting_datum: NodeType,
+    get_neighbors: Callable[[NodeType], Neighbors],
+    per_datum_work: Callable[[NodeType], None],
+    stopping_criterion: Callable[[NodeType], bool],
+) -> Iterable[NodeType]:
     def _bfs(
         *,
-        queue: list[Any],
-        get_neighbors: Callable[[Any], Neighbors],
-        per_datum_work: Callable[[Any], None],
-        stopping_criterion: Callable[[Any], bool],
-    ) -> Iterable[Any]:
+        queue: list[NodeType],
+        get_neighbors: Callable[[NodeType], Neighbors],
+        per_datum_work: Callable[[NodeType], None],
+        stopping_criterion: Callable[[NodeType], bool],
+    ) -> Iterable[NodeType]:
         while True:
             if len(queue) == 0:
                 break
@@ -47,34 +48,36 @@ def test_it() -> None:
     MAX_COORD = 2
     DIMENSIONS = 3
 
-    def get_neighbors(n: Any) -> Neighbors:
+    def get_neighbors(n: NodeType) -> Neighbors:
         def inc(x: int, delta: int) -> int:
             return min(MAX_COORD, x + delta)
 
         rv = []
-        gc = GrayCode(DIMENSIONS)
+        gc = GrayCode(DIMENSIONS)  # type: ignore
 
-        for offset in gc.generate_gray():
+        for offset in gc.generate_gray():  # type: ignore
             candidate = list(map(inc, n, [int(i) for i in offset]))
             if candidate != list(n):
                 rv.append(candidate)
         # print(f"{n=} neighbors={rv}")
         return rv
 
-    nodes_visited = []
+    nodes_visited: list[NodeType] = []
 
-    def stopping_criterion(n: Any) -> bool:
-        return len(nodes_visited) == pow((MAX_COORD + 1), DIMENSIONS)
+    def stopping_criterion(n: NodeType) -> bool:
+        return bool(len(nodes_visited) == pow((MAX_COORD + 1), DIMENSIONS))
 
-    def per_datum_work(datum: Any) -> None:
+    def per_datum_work(datum: NodeType) -> None:
         print(f"{datum=} sum:{sum(datum)}")
         nodes_visited.append(datum)
 
-    list(breadth_first_search(
-        starting_datum=[0] * DIMENSIONS,
-        get_neighbors=get_neighbors,
-        per_datum_work=per_datum_work,
-        stopping_criterion=stopping_criterion,
-    ))
+    list(
+        breadth_first_search(
+            starting_datum=[0] * DIMENSIONS,
+            get_neighbors=get_neighbors,
+            per_datum_work=per_datum_work,
+            stopping_criterion=stopping_criterion,
+        )
+    )
 
     assert len(nodes_visited) == pow((MAX_COORD + 1), DIMENSIONS)
