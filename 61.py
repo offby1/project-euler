71e87231d167987d3f288e4ab66ea65740697705
Eric Hanchrow <eric.hanchrow@gmail.com>
2023-07-16 09:00:26 -0700

    Tidy up

--- 61.py
+++ 61.py
@@ -1,31 +1,34 @@
 import collections
 import functools
 import itertools
+from typing import Callable
 
-import networkx as nx
+import networkx as nx  # type: ignore
 
 
 # https://en.wikipedia.org/wiki/Polygonal_number#Formula
-def P(s: int):
-    def formula(n):
+def P(s: int) -> Callable[[int], int]:
+    def formula(n: int) -> int:
         return ((s - 2) * n * n - (s - 4) * n) // 2
 
     return formula
 
 
 @functools.cache
-def polygonals_of_size(s: int):
-    rv = []
+def polygonals_of_size(s: int) -> list[int]:
+    rv: list[int] = []
     for n in itertools.count(1):
         p = P(s)(n)
         if p > 9999:
-            return rv
+            break
         elif 1_000 <= p:
-            rv.append (p)
+            rv.append(p)
+
+    return rv
 
 
 @functools.cache
-def four_digit_polygonals_of_size(s: int):
+def four_digit_polygonals_of_size(s: int) -> list[int]:
     rv = []
 
     for t in polygonals_of_size(s):
@@ -37,8 +40,12 @@ def four_digit_polygonals_of_size(s: int):
 
 
 if __name__ == "__main__":
-    fdns_by_size_and_first_two_digits = collections.defaultdict(lambda: collections.defaultdict(list))
-    fdns_by_size_and_last__two_digits = collections.defaultdict(lambda: collections.defaultdict(list))
+    fdns_by_size_and_first_two_digits = collections.defaultdict(  # type: ignore
+        lambda: collections.defaultdict(list)
+    )
+    fdns_by_size_and_last__two_digits = collections.defaultdict(  # type: ignore
+        lambda: collections.defaultdict(list)
+    )
     sizes_by_fdn = collections.defaultdict(set)
 
     for p_size in range(3, 9):
@@ -48,31 +55,22 @@ if __name__ == "__main__":
             fdns_by_size_and_first_two_digits[p_size][first].append(fdn)
             fdns_by_size_and_last__two_digits[p_size][last_].append(fdn)
 
-    # print(f"{len(sizes_by_fdn)=}")
-    # import pprint
-    # pprint.pprint(dict(fdns_by_size_and_first_two_digits))
-    # print(f"{fdns_by_size_and_last__two_digits=}")
-
     G = nx.DiGraph()
     for p_size, p_d in fdns_by_size_and_first_two_digits.items():
         for prefix, p_numbers in p_d.items():
             for p_n in p_numbers:
                 for s_size, d_d in fdns_by_size_and_last__two_digits.items():
                     for s_n in d_d[prefix]:
-                        print(f"{p_size=}: Adding edge from {s_n} to {p_n}")
                         G.add_edge(s_n, p_n)
-    print(G)
+
     for cycle in nx.simple_cycles(G, length_bound=6):
         if len(cycle) == 6:
             sizes_in_cycle = set()
             for n in cycle:
-                sizes_in_cycle.update(sizes_by_fdn[n])
+                sizes_in_cycle.add(frozenset(sizes_by_fdn[n]))
 
-            if sizes_in_cycle == {3, 4, 5, 6, 7, 8}:
-                for fdn in cycle:
-                    print(f"{fdn:04}: {sizes_by_fdn[fdn]}", end=", ")
-                print()
+            if len(sizes_in_cycle) == 6:
+                print(sum(cycle))
+                break
 
-    # This is the right answer:
     # 8256: {3}, 5625: {4}, 2512: {7}, 1281: {8}, 8128: {3, 6}, 2882: {5},
-    # TODO -- figure out how to identify it from amongst all the other six-cycles
